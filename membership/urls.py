3b6cc1cbce94ed655b2661db2bf2e75c462d2cc3
Muhammad Alif Bin Tajudin <mr.alif.93@gmail.com>
2023-07-02 02:05:33 +0800

    latest ui/ux

--- membership/urls.py
+++ membership/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView, name="index"),
+    path('', views.LoginView, name="login"),
+    path('logout/', views.LogoutView, name="logout"),
+    path('register/', views.RegisterView, name="register"),
+    path('index/', views.IndexView, name="index"),
+    path('profile/', views.ProfileView, name="profile"),
+    path('change-password/', views.ChangePasswordView, name="change_password"),
+    path('forgot-password/', views.ForgotPasswordView, name="forgot_password"),
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
+def LoginView(request):
+  context = {}
+  return render(request, 'membership/login.html', context)
+
+def LogoutView(request):
+  context = {}
+  return render(request, 'membership/register.html', context)
+
+def RegisterView(request):
+  context = {}
+  return render(request, 'membership/register.html', context)
+
 def IndexView(request):
-    return render(request, 'membership/index.html')
+  context = {}
+  return render(request, 'membership/index.html', context)
+
+def ProfileView(request):
+  context = {}
+  return render(request, 'membership/profile.html', context)
+
+def ChangePasswordView(request):
+  context = {}
+  return render(request, 'membership/change_password.html', context)
+
+def ForgotPasswordView(request):
+  context = {}
+  return render(request, 'membership/forgot_password.html', context)
