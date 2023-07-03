57cd241b17542894e6fb251c21eb201fe814d9d1
Muhammad Alif Bin Tajudin <mr.alif.93@gmail.com>
2023-07-04 02:50:43 +0800

    latest update db config

--- django_store_001/settings.py
+++ django_store_001/settings.py
@@ -99,15 +99,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
-# DATABASES['default'] = dj_database_url.config()
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
