9ad064aaba22d43bc18f2905957e98e5ba630360
Muhammad Alif Bin Tajudin <mr.alif.93@gmail.com>
2023-07-04 03:11:06 +0800

    code commit

--- django_store_001/settings.py
+++ django_store_001/settings.py
@@ -134,11 +134,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# static and media assets config
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'assets' / 'media/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'
+
+STATICFILES_DIRS = [
+	BASE_DIR / 'static',
+]
+if not DEBUG: STATICFILES_DIRS.append(MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
